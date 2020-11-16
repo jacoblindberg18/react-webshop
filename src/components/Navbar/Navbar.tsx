8a2833adb4a3d768d469e083773bf44910d61ced
jacoblindberg18 <jacob.lindberg18@gmail.com>
2020-11-16 12:43:10 +0100

    updated and removed unused dependencies

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
